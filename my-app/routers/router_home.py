b328ce631eb4fc35460f61a1d4deab5fa42d2085
Barcodehub <alex.barco.maicol@gmail.com>
2023-12-05 17:13:57 -0500

    diseño, avance en vista de user, funciones de busqueda para mesa, producto, arreglo de errores

--- my-app/routers/router_home.py
+++ my-app/routers/router_home.py
@@ -6,7 +6,6 @@ from mysql.connector.errors import Error
 # Importando cenexión a BD
 from controllers.funciones_home import *
 
-PATH_URL = "public/empleados"
 PATH_URL2 = "public/library"
 PATH_URL3 = "public/usuarios"
 PATH_URL4 = "public/productos"
@@ -98,86 +97,7 @@ def borrarProducto(id_producto, foto_producto):
 
 
 
-
-@app.route('/registrar-empleado', methods=['GET'])
-def viewFormEmpleado():
-    if 'conectado' in session:
-        return render_template(f'{PATH_URL}/form_empleado.html')
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-@app.route('/form-registrar-empleado', methods=['POST'])
-def formEmpleado():
-    if 'conectado' in session:
-        if 'foto_producto' in request.files:
-            foto_perfil = request.files['foto_producto']
-            resultado = procesar_form_empleado(request.form, foto_perfil)
-            if resultado:
-                return redirect(url_for('lista_empleados'))
-            else:
-                flash('El empleado NO fue registrado.', 'error')
-                return render_template(f'{PATH_URL}/form_empleado.html')
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-@app.route('/lista-de-empleados', methods=['GET'])
-def lista_empleados():
-    if 'conectado' in session:
-        return render_template(f'{PATH_URL}/lista_empleados.html', empleados=sql_lista_empleadosBD())
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-@app.route("/detalles-empleado/", methods=['GET'])
-@app.route("/detalles-empleado/<int:idEmpleado>", methods=['GET'])
-def detalleEmpleado(idEmpleado=None):
-    if 'conectado' in session:
-        # Verificamos si el parámetro idEmpleado es None o no está presente en la URL
-        if idEmpleado is None:
-            return redirect(url_for('inicio'))
-        else:
-            detalle_empleado = sql_detalles_empleadosBD(idEmpleado) or []
-            return render_template(f'{PATH_URL}/detalles_empleado.html', detalle_empleado=detalle_empleado)
-    else:
-        flash('Primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-# Buscadon de empleados
-@app.route("/buscando-empleado", methods=['POST'])
-def viewBuscarEmpleadoBD():
-    resultadoBusqueda = buscarEmpleadoBD(request.json['busqueda'])
-    if resultadoBusqueda:
-        return render_template(f'{PATH_URL}/resultado_busqueda_empleado.html', dataBusqueda=resultadoBusqueda)
-    else:
-        return jsonify({'fin': 0})
-
-
-@app.route("/editar-empleado/<int:id>", methods=['GET'])
-def viewEditarEmpleado(id):
-    if 'conectado' in session:
-        respuestaEmpleado = buscarEmpleadoUnico(id)
-        if respuestaEmpleado:
-            return render_template(f'{PATH_URL}/form_empleado_update.html', respuestaEmpleado=respuestaEmpleado)
-        else:
-            flash('El empleado no existe.', 'error')
-            return redirect(url_for('inicio'))
-    else:
-        flash('Primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-# Recibir formulario para actulizar informacion de empleado
-@app.route('/actualizar-empleado', methods=['POST'])
-def actualizarEmpleado():
-    resultData = procesar_actualizacion_form(request)
-    if resultData:
-        return redirect(url_for('lista_empleados'))
+#USUARIOS
 
 
 @app.route("/lista-de-usuarios", methods=['GET'])
@@ -223,21 +143,16 @@ def borrarUsuario(id):
         return redirect(url_for('usuarios'))
 
 
-@app.route('/borrar-empleado/<string:id_empleado>/<string:foto_producto>', methods=['GET'])
-def borrarEmpleado(id_empleado, foto_producto):
-    resp = eliminarEmpleado(id_empleado, foto_producto)
-    if resp:
-        flash('El Empleado fue eliminado correctamente', 'success')
-        return redirect(url_for('lista_empleados'))
 
+@app.route("/buscando-usuario", methods=['POST'])
+def viewBuscarUsuarioBD():
 
-@app.route("/descargar-informe-empleados/", methods=['GET'])
-def reporteBD():
-    if 'conectado' in session:
-        return generarReporteExcel()
+    resultadoBusqueda = buscarUsuarioBD(request.json['busqueda'])
+    if resultadoBusqueda:
+        print(resultadoBusqueda)
+        return render_template(f'{PATH_URL3}/resultado_busqueda_usuario.html', dataBusqueda=resultadoBusqueda)
     else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
+        return jsonify({'fin': 0})
 
 
 
@@ -287,6 +202,7 @@ def viewBuscarMesaBD():
 
     resultadoBusqueda = buscarMesaBD(request.json['busqueda'])
     if resultadoBusqueda:
+        print(resultadoBusqueda)
         return render_template(f'{PATH_URL5}/resultado_busqueda_mesa.html', dataBusqueda=resultadoBusqueda)
     else:
         return jsonify({'fin': 0})
@@ -310,7 +226,6 @@ def viewEditarMesa(id):
 # Recibir formulario para actulizar informacion de mesa
 @app.route('/actualizar-mesa', methods=['POST'])
 def actualizarMesa():
-    print("pasoooooooooooo")
     resultData = procesar_actualizacion_formMesa(request)
     if resultData:
         return redirect(url_for('lista_mesas'))
@@ -345,113 +260,6 @@ def borrarMesao(id_mesa):
 
 
 
-
-
-
-
-
-
-#LIBROS
-
-
-@app.route('/registrar-libro', methods=['GET'])
-def viewFormLibro():
-    if 'conectado' in session:
-        return render_template(f'{PATH_URL2}/form_libro.html')
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-
-
-@app.route('/form-registrar-libro', methods=['POST'])
-def formLibro():
-
-            resultado = procesar_form_libro(request.form)
-            if resultado:
-                return redirect(url_for('lista_libros'))
-            else:
-                flash('El libro NO fue registrado.', 'error')
-                return render_template(f'{PATH_URL2}/form_libro.html')
-
-@app.route('/lista-de-libros-user', methods=['GET'])
-def lista_libros2():
-    if 'conectado' in session:
-        return render_template(f'{PATH_URL2}/lista_libros2.html', libros=sql_lista_librosBD())
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-@app.route('/lista-de-libros', methods=['GET'])
-def lista_libros():
-    if 'conectado' in session:
-        return render_template(f'{PATH_URL2}/lista_libros.html', libros=sql_lista_librosBD())
-    else:
-        flash('primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-
-###############################
-
-@app.route("/detalles-libro/", methods=['GET'])
-@app.route("/detalles-libro/<int:idLibro>", methods=['GET'])
-def detalleLibro(idLibro=None):
-    if 'conectado' in session:
-        # Verificamos si el parámetro idLibro es None o no está presente en la URL
-        if idLibro is None:
-            return redirect(url_for('inicio'))
-        else:
-            detalle_libro = sql_detalles_librosBD(idLibro) or []
-            return render_template(f'{PATH_URL2}/detalles_libro.html', detalle_libro=detalle_libro)
-    else:
-        flash('Primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-
-###
-
-# Buscadon de libros
-@app.route("/buscando-libro", methods=['POST'])
-def viewBuscarLibroBD():
-
-    resultadoBusqueda = buscarLibroBD(request.json['busqueda'])
-    if resultadoBusqueda:
-        return render_template(f'{PATH_URL2}/resultado_busqueda_libro.html', dataBusqueda=resultadoBusqueda)
-    else:
-        return jsonify({'fin': 0})
-
-
-@app.route("/editar-libro/<int:id>", methods=['GET'])
-def viewEditarLibro(id):
-    if 'conectado' in session:
-        respuestaLibro = buscarLibroUnico(id)
-        if respuestaLibro:
-            return render_template(f'{PATH_URL2}/form_libro_update.html', respuestaLibro=respuestaLibro)
-        else:
-            flash('El libro no existe.', 'error')
-            return redirect(url_for('inicio'))
-    else:
-        flash('Primero debes iniciar sesión.', 'error')
-        return redirect(url_for('inicio'))
-
-
-# Recibir formulario para actulizar informacion de libro
-@app.route('/actualizar-libro', methods=['POST'])
-def actualizarLibro():
-    resultData = procesar_actualizacion_formlib(request)
-    if resultData:
-        return redirect(url_for('lista_libros'))
-
-@app.route('/borrar-libro/<string:id_libro>', methods=['GET'])
-def borrarLibro(id_libro):
-    resp = eliminarLibro(id_libro)
-    if resp:
-        flash('El Libro fue eliminado correctamente', 'success')
-        return redirect(url_for('lista_libros'))
 
 
 
--- my-app/static/assets/customJS/buscador.js
+++ my-app/static/assets/customJS/buscador.js
@@ -1,6 +1,46 @@
-async function buscadorTable(tableId) {
+
+
+async function buscadorTableProducto(tableId) {
   let input, busqueda, url;
-  url = "/buscando-empleado";
+  url = "/buscando-producto";
+
+  input = document.getElementById("search");
+  busqueda = input.value.toUpperCase();
+
+  const dataPeticion = { busqueda };
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  };
+
+  try {
+    const response = await axios.post(url, dataPeticion, { headers });
+    if (!response.status) {
+      console.log(`HTTP error! status: ${response.status} 😭`);
+    }
+
+    if (response.data.fin === 0) {
+      $(`#${tableId} tbody`).html("");
+      $(`#${tableId} tbody`).html(`
+      <tr>
+        <td colspan="6" style="text-align:center;color: red;font-weight: bold;">No resultados para la busqueda: <strong style="text-align:center;color: #222;">${busqueda}</strong></td>
+      </tr>`);
+      return false;
+    }
+
+    if (response.data) {
+      $(`#${tableId} tbody`).html("");
+      let miData = response.data;
+      $(`#${tableId} tbody`).append(miData);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function buscadorTableMesa(tableId) {
+  let input, busqueda, url;
+  url = "/buscando-mesa";
 
   input = document.getElementById("search");
   busqueda = input.value.toUpperCase();
@@ -37,17 +77,9 @@ async function buscadorTable(tableId) {
 }
 
 
-
-
-
-
-
-
-//libro
-
-async function buscadorTable2(tableId) {
+async function buscadorTableUser(tableId) {
   let input, busqueda, url;
-  url = "/buscando-libro";
+  url = "/buscando-usuario";
 
   input = document.getElementById("search");
   busqueda = input.value.toUpperCase();
@@ -82,5 +114,6 @@ async function buscadorTable2(tableId) {
     console.error(error);
   }
 }
+
 
 
