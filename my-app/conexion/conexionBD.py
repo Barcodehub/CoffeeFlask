640f9084ec21602453e9afce23c34c4a75bcc573
Brayan Alexander Barco <105529746+Barcodehub@users.noreply.github.com>
2024-06-17 20:13:51 -0500

    Update conexionBD.py

--- my-app/conexion/conexionBD.py
+++ my-app/conexion/conexionBD.py
@@ -1,30 +1,32 @@
-
-
-# Importando Libreria mysql.connector para conectar Python con MySQL
 import mysql.connector
 import os
 
-
 def connectionBD():
     try:
-        # connection = mysql.connector.connect(
+        host = "roundhouse.proxy.rlwy.net"
+        user = "root"
+        password = "PfZTYRCskvvFoLJFfFSSKsqKXQmAPxqu"
+        database = "railway"
+        port = 12437
+
         connection = mysql.connector.connect(
-           host=os.getenv("MYSQLHOST"),
-            user=os.getenv("MYSQLUSER"),
-            passwd=os.getenv("MYSQLPASSWORD"),
-            database=os.getenv("MYSQLDATABASE"),
-            port=int(os.getenv("MYSQLPORT", "12437")),
+            host=host,
+            user=user,
+            passwd=password,
+            database=database,
+            port=port,
             charset='utf8mb4',
             collation='utf8mb4_unicode_ci',
             raise_on_warnings=True
-
         )
+
         if connection.is_connected():
             print("Conexión exitosa a la BD")
             return connection
+        else:
+            print("No se pudo establecer la conexión con la base de datos.")
+            return None
 
     except mysql.connector.Error as error:
-        print(f"No se pudo conectar: {error}")
-
-    except mysql.connector.Error as error:
-        print(f"No se pudo conectar: {error}")
+        print(f"No se pudo conectar a MySQL: {error}")
+        return None
