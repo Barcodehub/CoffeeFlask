bb78ad14bc6e1b24d861209aa55ffe1825617285
Brayan Alexander Barco <105529746+Barcodehub@users.noreply.github.com>
2024-06-17 20:14:42 -0500

    Update run.py

--- my-app/run.py
+++ my-app/run.py
@@ -10,7 +10,7 @@ from routers.router_page_not_found import *
 
 # Ejecutando el objeto Flask
 if __name__ == '__main__':
-    #init_scheduler(app)
+    init_scheduler(app)
     port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
     #app.run(debug=True, port=5000)
